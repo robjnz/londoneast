85e646a1f0425d6f7c1a0e416bbfeac712203f14
robjnz <robjnz@outlook.com>
2018-09-29 09:48:22 +0100

    add files from dist

---
+++ public/_nuxt/pages_test.76d4645401c234bfcb49.js
@@ -0,0 +1,1 @@
+webpackJsonp([14],{"Bcm/":function(e,l,u){"use strict";Object.defineProperty(l,"__esModule",{value:!0});var n=u("VU/8")(null,null,!1,null,null,null);n.options.__file="pages/test.vue",l.default=n.exports}});
