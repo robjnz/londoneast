c25d49a8ecd6eeb0e3fa25c03f0d263e08c9d094
robjnz <robjnz@outlook.com>
2018-09-30 22:14:30 +0100

    up date travel

--- pages/news.vue
+++ pages/news.vue
@@ -3,8 +3,53 @@
   <b-container>
     <h1 class="title-color">News</h1>
     <div>
-        <b-card-group deck>
-    <b-card  bg-variant="dark" text-variant="white" title="Barking">
+      <div>
+        <!-- start sw-rss-feed code -->
+<script type="text/javascript">
+<!--
+            rssfeed_url                         = new Array();
+rssfeed_url[0]                                  = "http://feeds.bbci.co.uk/news/england/london/rss.xml";
+            rssfeed_frame_width                 = "400";
+            rssfeed_frame_height                = "260";
+            rssfeed_scroll                      = "on";
+            rssfeed_scroll_step                 = "6";
+            rssfeed_scroll_bar                  = "off";
+            rssfeed_target                      = "_blank";
+            rssfeed_font_size                   = "12";
+            rssfeed_font_face                   = "";
+            rssfeed_border                      = "on";
+            rssfeed_css_url                     = "https://feed.surfing-waves.com/css/style6.css";
+            rssfeed_title                       = "on";
+            rssfeed_title_name                  = "";
+            rssfeed_title_bgcolor               = "#3366ff";
+            rssfeed_title_color                 = "#fff";
+            rssfeed_title_bgimage               = "";
+            rssfeed_footer                      = "off";
+            rssfeed_footer_name                 = "rss feed";
+            rssfeed_footer_bgcolor              = "#fff";
+            rssfeed_footer_color                = "#333";
+            rssfeed_footer_bgimage              = "";
+            rssfeed_item_title_length           = "50";
+            rssfeed_item_title_color            = "#666";
+            rssfeed_item_bgcolor                = "#fff";
+            rssfeed_item_bgimage                = "";
+            rssfeed_item_border_bottom          = "on";
+            rssfeed_item_source_icon            = "off";
+            rssfeed_item_date                   = "off";
+            rssfeed_item_description            = "on";
+            rssfeed_item_description_length     = "120";
+            rssfeed_item_description_color      = "#666";
+            rssfeed_item_description_link_color = "#333";
+            rssfeed_item_description_tag        = "off";
+            rssfeed_no_items                    = "0";
+            rssfeed_cache                       = "add7bea72b872e1ffa8ffa1b3f8013b3";
+//-->
+</script>
+<script type="text/javascript" src="//feed.surfing-waves.com/js/rss-feed.js"></script>
+<!-- The link below helps keep this service FREE, and helps other people find the SW widget. Please be cool and keep it! Thanks. -->
+<div style="text-align:right; width:400px;">powered by <a href="http://www.surfing-waves.com" rel="noopener" target="_blank" style="color:#ccc;font-size:10px">Surfing Waves</a></div>
+<!-- end sw-rss-feed code -->
+      </div>
       <!-- start feedwind code --><script type="text/javascript">
   document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');
   </script><script type="text/javascript">
@@ -12,9 +57,8 @@
   </script>
   <div class="c5"><a href="http://feed.mikle.com/" target="_blank" class="c4"></a><!--Please display the above link in your web page according to Terms of Service.--></div>
   <!-- end feedwind code -->
-  </b-card>
 
-    <b-card  bg-variant="dark" text-variant="white" title="Barking">
+
 
         <!-- start feedwind code --><script type="text/javascript">
 document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');
@@ -23,13 +67,7 @@ document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document
 </script>
 <div class="c5"><a href="http://feed.mikle.com/" target="_blank" class="c4"></a><!--Please display the above link in your web page according to Terms of Service.--></div>
 <!-- end feedwind code -->
-
-  </b-card>
-        </b-card-group>
-
-
-  </div>
-
-  </b-container>
+    </div>
+</b-container>
 </section>
 </template>
