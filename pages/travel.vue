6b0b0426db8a9c5d530291e2f75a47884ae11d71
robjnz <robjnz@outlook.com>
2018-10-02 19:20:19 +0100

    update travel page

--- pages/travel.vue
+++ pages/travel.vue
@@ -432,14 +432,14 @@
     Live train times: overground stations.
   </p>
   <b-button-group>
-  <b-button href="#" target="_blank" variant="info">1</b-button>
-  <b-button href="#" target="_blank" variant="info">2</b-button>
+  <b-button href="http://bit.ly/2zLoLcg" target="_blank" variant="info">Chadwell Heath</b-button>
+  <b-button href="http://bit.ly/2P6jlhm" target="_blank" variant="info">Goodmayes</b-button>
   </b-button-group>
   <br>
   <br>
   <b-button-group>
-  <b-button href="#" target="_blank" variant="info">3</b-button>
-  <b-button href="#" target="_blank" variant="info">4</b-button>
+  <b-button href="http://bit.ly/2OuG9dH" target="_blank" variant="info">Ilford</b-button>
+  <b-button href="http://bit.ly/2y8YO4A" target="_blank" variant="info">Seven Kings</b-button>
   </b-button-group>
 
   </b-card>
@@ -449,36 +449,136 @@
     Live train times: underground stations.
     </p>
 <b-button-group>
-  <b-button href="#" target="_blank" variant="danger">1</b-button>
-  <b-button href="#" target="_blank" variant="danger">2</b-button>
-  <b-button href="#" target="_blank" variant="danger">3</b-button>
+  <b-button href="http://bit.ly/2y3UCTq" target="_blank" variant="danger">Barkingside</b-button>
+  <b-button href="http://bit.ly/2IBuULe" target="_blank" variant="danger">Chigwell</b-button>
+  <b-button href="http://bit.ly/2xSqYRM" target="_blank" variant="danger">Fairlop</b-button>
   </b-button-group>
   <br>
   <br>
   <b-button-group>
-  <b-button href="#" target="_blank" variant="danger">4</b-button>
-  <b-button href="#" target="_blank" variant="danger">5</b-button>
-  <b-button href="#" target="_blank" variant="danger">6</b-button>
+  <b-button href="http://bit.ly/2QoSBZA" target="_blank" variant="danger">Gants Hill</b-button>
+  <b-button href="http://bit.ly/2NWh4c2" target="_blank" variant="danger">Grange Hill</b-button>
+  <b-button href="http://bit.ly/2y6xYtV" target="_blank" variant="danger">Hainault</b-button>
   </b-button-group>
   <br>
   <br>
   <b-button-group>
-  <b-button href="#" target="_blank" variant="danger">7</b-button>
-  <b-button href="#" target="_blank" variant="danger">8</b-button>
-  <b-button href="#" target="_blank" variant="danger">9</b-button>
+  <b-button href="http://bit.ly/2NXlfEp" target="_blank" variant="danger">Newbury Park</b-button>
+  <b-button href="http://bit.ly/2NZEsp9" target="_blank" variant="danger">Redbridge</b-button>
+  <b-button href="http://bit.ly/2zMOEsx" target="_blank" variant="danger">Roding Valley</b-button>
   </b-button-group>
   <br>
   <br>
   <b-button-group>
-  <b-button href="#" target="_blank" variant="danger">10</b-button>
-  <b-button href="#" target="_blank" variant="danger">11</b-button>
-  <b-button href="#" target="_blank" variant="danger">12</b-button>
+  <b-button href="http://bit.ly/2y3VYgY" target="_blank" variant="danger">Snaresbrook</b-button>
+  <b-button href="http://bit.ly/2zMxRFL" target="_blank" variant="danger">South Woodford</b-button>
+  <b-button href="http://bit.ly/2xWlrtB" target="_blank" variant="danger">Wansted</b-button>
+  </b-button-group>
+  <br>
+  <br>
+  <b-button-group>
+  <b-button href="http://bit.ly/2xSMjKX" target="_blank" variant="danger">Woodford</b-button>
   </b-button-group>
   </b-card>
   </b-card-group>
     </section>
 
 <!--end redbridge -->
+
+<!-- tower hamlets -->
+    <section tower hamlets>
+      <b-card-group deck
+      class="mb-3">
+    <b-card  bg-variant="dark" text-variant="white" title="Tower Hamlets">
+  <p class="title-color">
+    Live train times: overground stations.
+  </p>
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="info">1</b-button>
+  <b-button href="#" target="_blank" variant="info">2</b-button>
+  <b-button href="#" target="_blank" variant="info">3</b-button>
+  </b-button-group>
+  <br>
+  <br>
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="info">4</b-button>
+  <b-button href="#" target="_blank" variant="info">5</b-button>
+  <b-button href="#" target="_blank" variant="info">6</b-button>
+  </b-button-group>
+  </b-card>
+
+<b-card  bg-variant="dark" text-variant="white" title="Tower Hamlets">
+  <p class="title-color">
+    Live train times: underground stations.
+    </p>
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="secondary">1</b-button>
+  <b-button href="#" target="_blank" variant="secondary">2</b-button>
+  <b-button href="#" target="_blank" variant="secondary">3</b-button>
+  </b-button-group>
+  <br>
+  <br>
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="secondary">4</b-button>
+  <b-button href="#" target="_blank" variant="secondary">5</b-button>
+  <b-button href="#" target="_blank" variant="secondary">6</b-button>
+  </b-button-group>
+  <br>
+  <br>
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="secondary">7</b-button>
+  <b-button href="#" target="_blank" variant="secondary">8</b-button>
+  <b-button href="#" target="_blank" variant="secondary">9</b-button>
+  </b-button-group>
+  <br>
+  <br>
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="secondary">10</b-button>
+  </b-button-group>
+  <br>
+  <br>
+  <p class="title-color">
+    Live train times: DLR stations.
+    </p>
+
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="info">1</b-button>
+  <b-button href="#" target="_blank" variant="info">2</b-button>
+  <b-button href="#" target="_blank" variant="info">3</b-button>
+  </b-button-group>
+  <br>
+  <br>
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="info">4</b-button>
+  <b-button href="#" target="_blank" variant="info">5</b-button>
+  <b-button href="#" target="_blank" variant="info">6</b-button>
+  </b-button-group>
+  <br>
+  <br>
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="info">7</b-button>
+  <b-button href="#" target="_blank" variant="info">8</b-button>
+  <b-button href="#" target="_blank" variant="info">9</b-button>
+  </b-button-group>
+  <br>
+  <br>
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="info">10</b-button>
+  </b-button-group>
+  <br>
+  <br>
+  <p class="title-color">
+    Live River Bus times.
+    </p>
+  <b-button-group>
+  <b-button href="#" target="_blank" variant="light">1</b-button>
+  <b-button href="#" target="_blank" variant="light">2</b-button>
+  </b-button-group>
+  </b-card>
+  </b-card-group>
+    </section>
+
+<!--end tower hamlets -->
 
 
 
