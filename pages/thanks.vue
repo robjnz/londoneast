45336e6a90ef5908aaf24cda4782e48c9aaf7d9a
robin harris <robjnz@outlook.com>
2018-11-01 17:05:39 +0000

    add thanks

---
+++ pages/thanks.vue
@@ -0,0 +1,50 @@
+<template>
+<section show class="showcase">
+  <div class="intro" >
+    <div class="row">
+    <h1>Information about what's going on in East London.</h1>
+    </div>
+  </div>
+
+  <div class="alert" >
+    <div class="row">
+    <h2>Museum of London Docklands. <br>free daily  10am - 6pm.</h2>
+        <b-button href="http://bit.ly/2EyBcwJ" target="_blank" variant="info">open</b-button>
+  <h2>01 November 2018</h2>
+    </div>
+  </div>
+  </section>
+</template>
+
+<style scoped>
+.showcase{
+  background             : url(http://res.cloudinary.com/dzhbfdfa5/image/upload/v1515831480/tower_vow6im.jpg) no-repeat center center fixed;
+  background-size        : cover;
+  -webkit-background-size: cover;
+  -moz-background-size   : cover;
+  -o-background-size     : cover;
+  height                 : 900px;
+  margin-top             : -75px;
+
+}
+.intro{
+  margin-left : 20px;
+  margin-right: 1000px;
+  margin-top  : 50px;
+  padding     : 30px;
+  color       : ghostwhite;
+  font-family : Damion;
+  }
+
+  .alert{
+  margin-left : 20px;
+  margin-right: 1000px;
+  margin-top  : 1px;
+  padding     : 30px;
+  color       : #171718;
+  font-family : Damion;
+  }
+
+
+</style>
+
