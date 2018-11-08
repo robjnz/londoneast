d7f3613664bf743730c6526db2c436a533f83209
robin harris <robjnz@outlook.com>
2018-11-08 16:07:22 +0000

    update contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,21 +7,22 @@
 <b-card-group deck
       class="mb-3">
 <b-card bg-variant="dark" text-variant="white" title= "">
-  <p class="title-color">
-
-<form  name="contact" method="POST" netlify>
+  <p class="title-color"></p>
+<form name="contact" method="POST" action="/thanks/" netlify>
   <p>
     <label>Your Name: <input type="text" name="name" /></label>
   </p>
   <p>
     <label>Your Email: <input type="email" name="email" /></label>
   </p>
+
   <p>
     <label>Message: <textarea name="message"></textarea></label>
   </p>
   <p>
-    <button type="submit">Send</button>
+    <button type="submit" value="Submit" id="Form-submit" >Submit</button>
   </p>
+
 </form>
 </b-card>
 
